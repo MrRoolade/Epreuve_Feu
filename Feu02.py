3283f6758ef3ea9ae5f236f4bebc7e6c8466ef8c
MrRoolade <128980419+MrRoolade@users.noreply.github.com>
2023-11-20 21:50:36 +0100

    still in WIP Feu02

--- Feu02.py
+++ Feu02.py
@@ -10,22 +10,36 @@ def handle_error(user_value):
 def quit_program(message):
     sys.exit(message)
 
-def define_map_board_list(name_of_file):
+def read_board(name_of_file):
     board_coordinates = []
-    with open(name_of_file, 'r') as f:
-        for line  in f:
-            line_array = [ x for x in line if x != "\n"]
-            board_coordinates.append(line_array)
-    return board_coordinates
+    try:
+        with open(name_of_file, 'r') as f:
+            for line  in f:
+                line_array = [ x for x in line if x != "\n"]
+                board_coordinates.append(line_array)
+        return board_coordinates
+    except :
+        quit_program("le fichier 1 n'existe pas")
 
-def define_map_pattern_list(name_of_file):
+def read_pattern(name_of_file):
     pattern_coordinates = []
-    with open(name_of_file, 'r') as f:
-        for line in f:
-            line_array = [ x for x in line if x != "\n"  ]
-            pattern_coordinates.append(line_array)
-    return pattern_coordinates
+    try:
+        with open(name_of_file, 'r') as f:
+            for line in f:
+                line_array = [ x for x in line.replace(" ","-") if x != "\n" ]
+                pattern_coordinates.append(line_array)
+            print(pattern_coordinates)
+        return pattern_coordinates
+    except: 
+        quit_program("le fichier 2 n'existe pas")
 
+def check_pattern(board,pattern, x, y):
+    for yp in range(len(pattern)):
+        for xp in range(len(pattern[yp])):
+            if pattern[yp][xp] != "-" and board[y + yp][x + xp] != pattern[yp][xp]:
+                return False
+    return True
+           
 def find_pattern_position(board, pattern):
     for y in range(len(board)-len(pattern)+1):
         for x in range(len(board[y])-len(pattern[y])+1):
@@ -33,35 +47,51 @@ def find_pattern_position(board, pattern):
                 return x, y
     return None, None
 
-def check_pattern(board,pattern, x, y):
-    for yp in range(len(pattern)):
-        for xp in range(len(pattern[yp])):
-            if pattern[yp][xp] != " " and board[y + yp][x + xp] != pattern[yp][xp]:
-                return False
-    return True
-           
-def display_result(result, x, y):
+def display_result(result, x, y, board, pattern):
     if result:
         print ("Trouvé", end = "\n")
         print (f'coordonnées : ({x}, {y})')
-
+        display = display_pattern_in_board(board,pattern, x, y)
+        print (display)
         return True  # Motif trouvé à la position (y, x)
     else:
         print ("PAS Trouvé", end = "\n")
         return False  # Motif non trouvé dans le tableau 
     
+def display_pattern_in_board(board,pattern, x, y):
+    display = []
+    for yb in range(len(board)):
+        line = []
+        for xb in range(len(board[yb])):
+            if  x <= xb < x + max(len(row) for row in pattern) and y <= yb < y +len(pattern) :
+                yp = yb - y
+                xp = xb - x
+                if yp < len(pattern) and xp < len(pattern[yp]):
+                    if pattern[yp][xp] != "-" :
+                        line.append(pattern[yp][xp])
+                    else:
+                        line.append("-")
+                else:
+                    line.append("-")
+            else:
+                line.append("-")
+        line = "".join(line)
+        display.append(line)
+    display = "\n".join(display)
+    return display
+
 def check_pattern_in_board(board,pattern):
     x, y = find_pattern_position(board, pattern)
-    display_result(x is not None and y is not None, x, y)
+    display_result(x is not None and y is not None, x, y,board,pattern)
     return x is not None and y is not None
 
 def main():
+    name_of_file_board = sys.argv[1]
+    name_of_file_pattern = sys.argv[2]
     handle_error(sys.argv)
-    name_of_file_board = "board.txt"
-    name_of_file_pattern = "to_find.txt"
 
-    board_list = define_map_board_list(name_of_file_board)
-    pattern_list = define_map_pattern_list(name_of_file_pattern)
+    board_list = read_board(name_of_file_board)
+    pattern_list = read_pattern(name_of_file_pattern)
 
     result = check_pattern_in_board(board_list,pattern_list)
    
