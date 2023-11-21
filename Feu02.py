1b74f3fe3a0a9d344f37d3897f73b4bce7e536ee
MrRoolade <128980419+MrRoolade@users.noreply.github.com>
2023-11-21 09:22:19 +0100

    final release Feu02

--- Feu02.py
+++ Feu02.py
@@ -28,18 +28,20 @@ def read_pattern(name_of_file):
             for line in f:
                 line_array = [ x for x in line.replace(" ","-") if x != "\n" ]
                 pattern_coordinates.append(line_array)
-            print(pattern_coordinates)
         return pattern_coordinates
     except: 
         quit_program("le fichier 2 n'existe pas")
 
+# Vérifie si le motif est présent dans le plateau à la position (x, y)
 def check_pattern(board,pattern, x, y):
     for yp in range(len(pattern)):
         for xp in range(len(pattern[yp])):
+            # Vérifie si la position du motif ne correspond pas à celle du plateau
             if pattern[yp][xp] != "-" and board[y + yp][x + xp] != pattern[yp][xp]:
                 return False
     return True
-           
+
+# Trouve la position du motif dans le plateau
 def find_pattern_position(board, pattern):
     for y in range(len(board)-len(pattern)+1):
         for x in range(len(board[y])-len(pattern[y])+1):
@@ -47,43 +49,46 @@ def find_pattern_position(board, pattern):
                 return x, y
     return None, None
 
-def display_result(result, x, y, board, pattern):
-    if result:
+# Vérifie si le motif est présent dans le plateau
+def check_pattern_in_board(board,pattern):
+    x, y = find_pattern_position(board, pattern)
+    if x is not None and y is not None:
         print ("Trouvé", end = "\n")
         print (f'coordonnées : ({x}, {y})')
         display = display_pattern_in_board(board,pattern, x, y)
-        print (display)
-        return True  # Motif trouvé à la position (y, x)
+        return display  # Motif trouvé à la position (y, x)
     else:
-        print ("PAS Trouvé", end = "\n")
-        return False  # Motif non trouvé dans le tableau 
+        display = "PAS Trouvé"
+        return display  # Motif non trouvé dans le tableau
     
+# Construit une représentation du plateau avec le motif trouvé à la position (x, y)
 def display_pattern_in_board(board,pattern, x, y):
     display = []
-    for yb in range(len(board)):
+    for yb, row in enumerate(board):
         line = []
-        for xb in range(len(board[yb])):
-            if  x <= xb < x + max(len(row) for row in pattern) and y <= yb < y +len(pattern) :
+        for xb, _ in enumerate(row):# "_" est utilisé pour représenter une variable qui n'est pas utilisée dans le contexte
+            # Vérifie si la position (x, y) se trouve dans la zone où le motif doit être affiché
+            x_pattern =  x <= xb < x + max(len(row) for row in pattern)
+            y_pattern =  y <= yb < y + len(pattern) 
+
+            # Calcule les coordonnées relatives du motif par rapport au coin supérieur gauche du pattern
+            if x_pattern and y_pattern :
                 yp = yb - y
                 xp = xb - x
-                if yp < len(pattern) and xp < len(pattern[yp]):
-                    if pattern[yp][xp] != "-" :
-                        line.append(pattern[yp][xp])
-                    else:
-                        line.append("-")
+
+                # Vérifie si la position se trouve dans les limites du motif et récupère le caractère correspondant
+                if yp < len(pattern) and xp < len(pattern[yp]) and pattern[yp][xp] != "-" :
+                    line.append(pattern[yp][xp])
                 else:
                     line.append("-")
             else:
                 line.append("-")
+
         line = "".join(line)
         display.append(line)
+        
     display = "\n".join(display)
     return display
-
-def check_pattern_in_board(board,pattern):
-    x, y = find_pattern_position(board, pattern)
-    display_result(x is not None and y is not None, x, y,board,pattern)
-    return x is not None and y is not None
 
 def main():
     name_of_file_board = sys.argv[1]
@@ -94,7 +99,7 @@ def main():
     pattern_list = read_pattern(name_of_file_pattern)
 
     result = check_pattern_in_board(board_list,pattern_list)
+    print(result)
    
-
 if __name__ == "__main__":
     main()
