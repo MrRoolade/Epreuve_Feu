1ace9b81da2e32788382df07b7baecad6e19fc94
MrRoolade <romain.recasens@free.fr>
2024-01-30 18:33:40 +0100

    WIP sudoku

--- Feu03_copy.py
+++ Feu03_copy.py
@@ -2,6 +2,7 @@
 # Créez un programme qui trouve et affiche la solution d’un Sudoku.
 
 import sys
+import copy
 
 def handle_error(user_value): 
     if len(user_value)!=2: 
@@ -15,7 +16,7 @@ def read_rows(name_of_file):
     try:
         with open(name_of_file, 'r') as f:
             for line  in f:
-                line_array = [ x.replace(".","0") for x in line if x != "\n"]
+                line_array = [ x.replace(".", "0" ) for x in line if x != "\n"]
                 list_rows.append(line_array)
         return list_rows
     except :
@@ -32,55 +33,69 @@ def display_board(board):
             if j == 8:
                 print(board[i][j])
             else:
-                print(str(board[i][j]) + "", end="")
+                print(str(board[i][j]), end="")
 
 def check_number(x,y,n,temp_board):
 
-    for i in range(0, 9):
-        if temp_board[i][x] == n and i != y:
+    for i in range(9):
+        if temp_board[i][x] == n and i != y and temp_board[i][x] != 0:
             return False
 
-    for i in range(0, 9):
-        if temp_board[y][i] == n and i != x: 
+    for i in range(9):
+        if temp_board[y][i] == n and i != x and temp_board[y][i] != 0: 
             return False
 
     x0 = (x // 3) * 3 #trouve le coin des carrés intermédiaires
     y0 = (y // 3) * 3 #trouve le coin des carrés intermédiaires
     for xi in range(x0, x0 + 3):
         for yi in range(y0, y0 + 3):
-            if temp_board[yi][xi] == n:
+            if temp_board[yi][xi] == n and temp_board[yi][xi] != 0:
                 return False    
     return True
 
 def solve(temp_board):
-    possible_tree=[]
-    for y in range(0,9):
-        for x in range(0,9):
-            if temp_board[y][x] == 0 : 
-                for n in range(1,10):
-                    if check_number(x, y, n,temp_board):
-                        temp_board[y][x] = n
-                        possible_tree.append(temp_board.copy())
-                        if temp_board.count(0) != 0 :
-                            solve(possible_tree[-1])
-                        # condition d'arret / versioning de board
-                    else:
-                        temp_board[y][x] = 0
-                        temp_board = possible_tree.pop()
-                        
+    empty_cell = find_empty_cell(temp_board)
+    if not empty_cell:
+        return temp_board  # La grille est déjà complète
+    
+    y, x = empty_cell
+    
+    for n in range(1, 10):
+        if check_number(x, y, n, temp_board):
+            temp_board[y][x] = n
+            
+            if solve(temp_board):
                 return temp_board
+    
+            temp_board[y][x] = 0  # Revert if none of the choices lead to a solution
+    
+    return None
+
+def find_empty_cell(board):
+    for i in range(9):
+        for j in range(9):
+            if int(board[i][j])== 0:
+                return (i, j)
+    return None
 
 def main():
-    name_of_file_board = "s1.txt"
+    name_of_file_board = "s.txt"
     handle_error(sys.argv)
-    board = read_rows(name_of_file_board)
-    print(board)
-    display_board(board)
-    print(11*"=")
-    result = solve(board)
-    print(result)
+    try:
+        board = read_rows(name_of_file_board)
+        
+        print("grille de départ")
+        display_board(board)
+        print(11*"=")
 
-    display_board(result)
+        result = solve(copy.deepcopy(board))
+        if result:
+            print("solution trouvée")
+            display_board(result)
+        else:
+            print("aucune solution trouvée")
+    except Exception as e:
+        quit_program(f"une erreur s'est produite : {str(e)}")
 
 
 if __name__ == "__main__":
